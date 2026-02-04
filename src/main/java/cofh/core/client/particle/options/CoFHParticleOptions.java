035e335a89f114fdb1c5ab078a6b7605966ce08c
noob990060 <noob990060@gmail.com>
2026-02-05 00:33:47 +0100

    Migrate CoFHParticleOptions to 1.21+ Error fix is on hold for this file

--- src/main/java/cofh/core/client/particle/options/CoFHParticleOptions.java
+++ src/main/java/cofh/core/client/particle/options/CoFHParticleOptions.java
@@ -1,68 +1,61 @@
 package cofh.core.client.particle.options;
 
-import com.mojang.brigadier.StringReader;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleType;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.codec.StreamCodec;
+
+import java.util.function.Function;
 
 public class CoFHParticleOptions implements ParticleOptions {
 
-    public final ParticleType<? extends CoFHParticleOptions> type;
+    protected final ParticleType<? extends CoFHParticleOptions> type;
     public final float size;
     public final float duration;
     public final float delay;
 
-    public CoFHParticleOptions(ParticleType<? extends CoFHParticleOptions> type, float size, float duration, float delay) {
-
+    public CoFHParticleOptions(ParticleType<? extends CoFHParticleOptions> type,
+            float size, float duration, float delay) {
         this.type = type;
         this.size = size;
         this.duration = duration;
         this.delay = delay;
     }
 
-    public CoFHParticleOptions(ParticleType<? extends CoFHParticleOptions> type, float size, float duration) {
-
-        this.type = type;
-        this.size = size;
-        this.duration = duration;
-        this.delay = 0.0F;
+    public CoFHParticleOptions(ParticleType<? extends CoFHParticleOptions> type,
+            float size, float duration) {
+        this(type, size, duration, 0.0F);
     }
 
-    public CoFHParticleOptions(ParticleType<? extends ColorParticleOptions> type) {
-
+    public CoFHParticleOptions(ParticleType<? extends CoFHParticleOptions> type) {
         this(type, 1.0F, 1.0F, 0.0F);
-    }
-
-    protected CoFHParticleOptions(ParticleType<? extends CoFHParticleOptions> type, StringReader reader) throws CommandSyntaxException {
-
-        this.type = type;
-        reader.expect(' ');
-        this.size = (float) reader.readDouble();
-        reader.expect(' ');
-        this.duration = (float) reader.readDouble();
-        reader.expect(' ');
-        this.delay = (float) reader.readDouble();
     }
 
     @Override
     public ParticleType<? extends CoFHParticleOptions> getType() {
-
         return type;
     }
 
-    @Override
-    public void writeToNetwork(FriendlyByteBuf buf) {
+    /* ---------------- Serialization ---------------- */
 
-        buf.writeFloat(size);
-        buf.writeFloat(duration);
-        buf.writeFloat(delay);
-    }
+    public static final Function<ParticleType<CoFHParticleOptions>, Codec<CoFHParticleOptions>> CODEC = type -> RecordCodecBuilder
+            .create(builder -> builder.group(
+                    Codec.FLOAT.fieldOf("size").forGetter(o -> o.size),
+                    Codec.FLOAT.fieldOf("duration").forGetter(o -> o.duration),
+                    Codec.FLOAT.fieldOf("delay").forGetter(o -> o.delay)).apply(builder,
+                            (size, duration, delay) -> new CoFHParticleOptions(type, size, duration, delay)));
 
-    @Override
-    public String writeToString() {
-
-        return size + ", " + duration + ", " + delay;
-    }
-
+    public static final StreamCodec<FriendlyByteBuf, CoFHParticleOptions> STREAM_CODEC = StreamCodec.of(
+            (buf, o) -> {
+                buf.writeFloat(o.size);
+                buf.writeFloat(o.duration);
+                buf.writeFloat(o.delay);
+            },
+            buf -> new CoFHParticleOptions(
+                    ModParticles.COFH.get(), // registry object
+                    buf.readFloat(),
+                    buf.readFloat(),
+                    buf.readFloat()));
 }
