3cdde52a5a2588ceeadef2c6fc17259162def0da
noob990060 <noob990060@gmail.com>
2026-02-05 10:24:31 +0100

    Migrate MistParticle to 1.21+

--- src/main/java/cofh/core/client/particle/impl/MistParticle.java
+++ src/main/java/cofh/core/client/particle/impl/MistParticle.java
@@ -9,6 +9,8 @@ import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.ParticleProvider;
 import net.minecraft.client.particle.SpriteSet;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.util.Mth;
+import net.minecraft.world.phys.Vec3;
 
 import javax.annotation.Nonnull;
 
@@ -32,7 +34,47 @@ public class MistParticle extends GasParticle {
 
         //Only set render size based off BB size
         this.size = this.bbWidth * MathHelper.sin(0.25F * MathHelper.F_PI * (progress + 1));
-        super.render(stack, buffer, consumer, packedLight, time, pTicks);
+        
+        // Use the same rendering logic as SpriteParticle but with the provided parameters
+        Vec3 camPos = new Vec3(0, 0, 0); // Camera position is already accounted for in the CoFHParticle.render method
+        
+        float x = (float) (Mth.lerp(pTicks, xo, this.x) - camPos.x);
+        float y = (float) (Mth.lerp(pTicks, yo, this.y) - camPos.y);
+        float z = (float) (Mth.lerp(pTicks, zo, this.z) - camPos.z) + 0.1F;
+
+        float rot = MathHelper.interpolate(oRoll, roll, pTicks);
+        float sin = MathHelper.sin(rot);
+        float cos = MathHelper.cos(rot);
+        float w = size * 0.5F;
+        float a = w * (cos - sin);
+        float b = w * (sin + cos);
+
+        float u0 = sprite.getU0();
+        float u1 = sprite.getU1();
+        float v0 = sprite.getV0();
+        float v1 = sprite.getV1();
+
+        int light = getLightColor(pTicks);
+
+        consumer.addVertex(x + a, y + b, z);
+        consumer.setUv(u1, v0);
+        consumer.setColor(c0.r, c0.g, c0.b, c0.a);
+        consumer.setLight(light);
+
+        consumer.addVertex(x - b, y + a, z);
+        consumer.setUv(u0, v0);
+        consumer.setColor(c0.r, c0.g, c0.b, c0.a);
+        consumer.setLight(light);
+
+        consumer.addVertex(x - a, y - b, z);
+        consumer.setUv(u0, v1);
+        consumer.setColor(c0.r, c0.g, c0.b, c0.a);
+        consumer.setLight(light);
+
+        consumer.addVertex(x + b, y - a, z);
+        consumer.setUv(u1, v1);
+        consumer.setColor(c0.r, c0.g, c0.b, c0.a);
+        consumer.setLight(light);
     }
 
     @Nonnull
